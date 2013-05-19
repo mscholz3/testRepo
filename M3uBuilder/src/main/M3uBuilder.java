f8d50a90c55b63ff8f5997d4aa8dbf578cce4ac3
Micha <Micha@fake.com>
2013-05-20 01:24:35 +0200

    Add a new method

--- M3uBuilder/src/main/M3uBuilder.java
+++ M3uBuilder/src/main/M3uBuilder.java
@@ -4,33 +4,53 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class M3uBuilder extends Object{
-	Map<String, Map<String, Set<String>>> series = new HashMap<>();
+public class M3uBuilder extends Object {
+	Map<String, Map<String, Set<String>>>	series	= new HashMap<>();
 
-	public M3uBuilder(Map<String, Map<String, Set<String>>> series) {
+	public M3uBuilder(final Map<String, Map<String, Set<String>>> series) {
 		this.series = series;
 	}
 
 	public Set<String> getSeries() {
-		if (series.isEmpty())
+		if (series.isEmpty()) {
 			return null;
+		}
 
 		return series.keySet();
 	}
 
 	public Set<String> getSeasons(final String serie) {
-		if (series.isEmpty())
+		if (series.isEmpty()) {
 			return null;
+		}
 
-		if (!series.containsKey(serie))
+		if (!series.containsKey(serie)) {
 			return null;
+		}
 
-		Map<String, Set<String>> seasons = series.get(serie);
+		final Map<String, Set<String>> seasons = series.get(serie);
 		return seasons.keySet();
 	}
 
-	public Set<String> getEpisodes(String serie, String season) {
-		
-		return null;
+	public Set<String> getEpisodes(final String serie, final String season) {
+		if (series.isEmpty()) {
+			return null;
+		}
+
+		if (!series.containsKey(serie)) {
+			return null;
+		}
+
+		final Map<String, Set<String>> seasonsTemp = series.get(serie);
+
+		if (seasonsTemp.isEmpty()) {
+			return null;
+		}
+
+		if (!seasonsTemp.containsKey(season)) {
+			return null;
+		}
+
+		return seasonsTemp.get(season);
 	}
 }
--- M3uBuilder/test/testmain/M3uBuilderTest.java
+++ M3uBuilder/test/testmain/M3uBuilderTest.java
@@ -52,13 +52,13 @@ public class M3uBuilderTest {
 	public void testM3uBuilder() {
 		final Set<String> seriesTest = m3uToTest.getSeries();
 		final Set<String> seasonsTest = m3uToTest.getSeasons("Firefly");
-		// final Set<String> episodesTest = m3uToTest.getEpisodes("Firefly",
-		// "Season 1");
+		final Set<String> episodesTest = m3uToTest.getEpisodes("Firefly",
+			"Season 1");
 		assertEquals("Keysets of the series should be the same",
 			testSeries.keySet(), seriesTest);
 		assertEquals("Keysets of the seasons should be the same",
 			testSeasons.keySet(), seasonsTest);
-		// assertEquals("Sets of the episodes should be the same", testEpisodes,
-		// episodesTest);
+		assertEquals("Sets of the episodes should be the same", testEpisodes,
+			episodesTest);
 	}
 }
